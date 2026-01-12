16d7ab3b3a5a693eac47e727d5daa03e46279532
sahil <sahhillyddvv@gmail.com>
2026-01-12 09:57:52 +0530

    add Conversation and Message schemas

---
+++ backend/src/models/Conversation.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const conversationSchema = new mongoose.Schema({
+    participants:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }],
+    lastMessage:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Message" //it is for the small message window
+    }
+},{
+    timestamps:true
+})
+
+export default mongoose.model("Conversation",conversationSchema);
---
+++ backend/src/models/Message.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    conversationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Conversation",
+      required: true,
+    },
+
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    messageType: {
+      type: String,
+      enum: ["text", "image", "video"],
+      default: "text",
+    },
+
+    content: {
+      type: String,
+      required: true,
+    },
+
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Message", messageSchema);
